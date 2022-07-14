d13686f429697751bb8f0daf1c5c0cbfef387671
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-14 19:29:34 +0200

    feat: exercise 3.3

--- src/test/kotlin/chapter3/exercises/ex3/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex3/listing.kt
@@ -1,31 +1,36 @@
 package chapter3.exercises.ex3
 
+import chapter3.Cons
 import chapter3.List
 import chapter3.Nil
 import io.kotlintest.shouldBe
 import io.kotlintest.shouldThrow
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 // tag::init[]
-fun <A> drop(l: List<A>, n: Int): List<A> =
-
-    SOLUTION_HERE()
+tailrec fun <A> drop(l: List<A>, n: Int): List<A> =
+    if (n == 0) l
+    else when (l) {
+        is Nil -> throw IllegalStateException(
+            "Cannot `drop` more elements than in list"
+        )
+        is Cons -> drop(l.tail, n - 1)
+    }
 // end::init[]
 
-//TODO: Enable tests by removing `!` prefix
+// TODO: Enable tests by removing `!` prefix
 class Exercise3 : WordSpec({
     "list drop" should {
-        "!drop a given number of elements within capacity" {
+        "drop a given number of elements within capacity" {
             drop(List.of(1, 2, 3, 4, 5), 3) shouldBe
                 List.of(4, 5)
         }
 
-        "!drop a given number of elements up to capacity" {
+        "drop a given number of elements up to capacity" {
             drop(List.of(1, 2, 3, 4, 5), 5) shouldBe Nil
         }
 
-        """!throw an illegal state exception when dropped elements
+        """throw an illegal state exception when dropped elements
             exceed capacity""" {
             shouldThrow<IllegalStateException> {
                 drop(List.of(1, 2, 3, 4, 5), 6)
