551de6de0fa15ecfe4c8acfd5460604d15c181a5
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-16 14:39:28 +0200

    feat: exercise 4.3

--- src/test/kotlin/chapter4/exercises/ex3/listing.kt
+++ src/test/kotlin/chapter4/exercises/ex3/listing.kt
@@ -5,15 +5,20 @@ import chapter4.Option
 import chapter4.Some
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
-//tag::init[]
+// tag::init[]
 fun <A, B, C> map2(a: Option<A>, b: Option<B>, f: (A, B) -> C): Option<C> =
+    when (a) {
+        is None -> None
+        is Some ->
+            when (b) {
+                is None -> None
+                is Some -> Some(f(a.get, b.get))
+            }
+    }
+// end::init[]
 
-    SOLUTION_HERE()
-//end::init[]
-
-//TODO: Enable tests by removing `!` prefix
+// TODO: Enable tests by removing `!` prefix
 class Exercise3 : WordSpec({
 
     "map2" should {
@@ -22,13 +27,13 @@ class Exercise3 : WordSpec({
         val b = Some(20)
         val none = Option.empty<Int>()
 
-        "!combine two option values using a binary function" {
+        "combine two option values using a binary function" {
             map2(a, b) { aa, bb ->
                 aa * bb
             } shouldBe Some(100)
         }
 
-        "!return none if either option is not defined" {
+        "return none if either option is not defined" {
             map2(a, none) { aa, bb ->
                 aa * bb
             } shouldBe None
