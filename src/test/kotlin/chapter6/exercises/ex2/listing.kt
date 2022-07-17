ccde19014b43ee1df60de7c7bf1c34e03d1eddb6
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-17 19:23:20 +0200

    feat: exercise 6.2

--- src/test/kotlin/chapter6/exercises/ex2/listing.kt
+++ src/test/kotlin/chapter6/exercises/ex2/listing.kt
@@ -1,19 +1,20 @@
 package chapter6.exercises.ex2
 
+// import chapter6.solutions.ex1.nonNegativeInt
 import chapter6.RNG
-//import chapter6.solutions.ex1.nonNegativeInt
+import chapter6.solutions.ex1.nonNegativeInt
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
-//TODO: Enable tests by removing `!` prefix
+// TODO: Enable tests by removing `!` prefix
 class Exercise2 : WordSpec({
 
-    //tag::init[]
-    fun double(rng: RNG): Pair<Double, RNG> =
-
-        SOLUTION_HERE()
-    //end::init[]
+    // tag::init[]
+    fun double(rng: RNG): Pair<Double, RNG> {
+        val (i, rng2) = nonNegativeInt(rng)
+        return (i / (Int.MAX_VALUE.toDouble() + 1)) to rng2
+    }
+    // end::init[]
 
     "double" should {
 
@@ -21,7 +22,7 @@ class Exercise2 : WordSpec({
             override fun nextInt(): Pair<Int, RNG> = TODO()
         }
 
-        "!generate a max value approaching 1 based on Int.MAX_VALUE" {
+        "generate a max value approaching 1 based on Int.MAX_VALUE" {
 
             val rngMax = object : RNG {
                 override fun nextInt(): Pair<Int, RNG> =
@@ -31,7 +32,7 @@ class Exercise2 : WordSpec({
             double(rngMax) shouldBe (0.9999999995343387 to unusedRng)
         }
 
-        "!generate a min value of 0 based on 0" {
+        "generate a min value of 0 based on 0" {
             val rngMin = object : RNG {
                 override fun nextInt(): Pair<Int, RNG> =
                     0 to unusedRng
