ecd934a60ad9e1cfc95c5271e75b148b960a7ada
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-17 15:50:36 +0200

    feat: exercise 5.11

--- src/test/kotlin/chapter5/exercises/ex11/listing.kt
+++ src/test/kotlin/chapter5/exercises/ex11/listing.kt
@@ -3,23 +3,26 @@ package chapter5.exercises.ex11
 import chapter3.List
 import chapter4.Option
 import chapter4.Some
+import chapter4.getOrElse
 import chapter5.Stream
-import chapter5.toList
+import chapter5.Stream.Companion.cons
+import chapter5.Stream.Companion.empty
 import chapter5.solutions.ex13.take
+import chapter5.toList
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
-//tag::init[]
+// tag::init[]
 fun <A, S> unfold(z: S, f: (S) -> Option<Pair<A, S>>): Stream<A> =
+    f(z).map { pair ->
+        cons({ pair.first }, { unfold(pair.second, f) })
+    }.getOrElse { empty() }
+// end::init[]
 
-    SOLUTION_HERE()
-//end::init[]
-
-//TODO: Enable tests by removing `!` prefix
+// TODO: Enable tests by removing `!` prefix
 class Exercise11 : WordSpec({
     "unfold" should {
-        """!return a stream based on an initial state and a function
+        """return a stream based on an initial state and a function
             applied to each subsequent element""" {
             unfold(0, { s: Int ->
                 Some(s to (s + 1))
