6b9cc1804596b736ffee7720aaa21ddeeedcce7e
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-17 16:49:53 +0200

    feat: exercise 5.16

--- src/test/kotlin/chapter5/exercises/ex16/listing.kt
+++ src/test/kotlin/chapter5/exercises/ex16/listing.kt
@@ -5,19 +5,24 @@ import chapter5.Stream
 import chapter5.toList
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
-//TODO: Enable tests by removing `!` prefix
+// TODO: Enable tests by removing `!` prefix
 class Exercise16 : WordSpec({
 
-    //tag::scanright[]
+    // tag::scanright[]
     fun <A, B> Stream<A>.scanRight(z: B, f: (A, () -> B) -> B): Stream<B> =
-
-        SOLUTION_HERE()
-    //end::scanright[]
+        foldRight(
+            { z to Stream.of(z) },
+            { a: A, p0: () -> Pair<B, Stream<B>> ->
+                val p1: Pair<B, Stream<B>> by lazy { p0() }
+                val b2: B = f(a) { p1.first }
+                Pair(b2, Stream.cons({ b2 }, { p1.second }))
+            }
+        ).second
+    // end::scanright[]
 
     "Stream.scanRight" should {
-        "!behave like foldRight" {
+        "behave like foldRight" {
             Stream.of(1, 2, 3)
                 .scanRight(0, { a, b ->
                     a + b()
