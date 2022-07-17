d2cddf31e76b52225a0a2e224e282590e1bce80f
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-17 19:18:17 +0200

    feat: exercise 6.1

--- src/test/kotlin/chapter6/exercises/ex1/listing.kt
+++ src/test/kotlin/chapter6/exercises/ex1/listing.kt
@@ -3,16 +3,16 @@ package chapter6.exercises.ex1
 import chapter6.RNG
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
-//TODO: Enable tests by removing `!` prefix
+// TODO: Enable tests by removing `!` prefix
 class Exercise1 : WordSpec({
 
-    //tag::init[]
-    fun nonNegativeInt(rng: RNG): Pair<Int, RNG> =
-
-        SOLUTION_HERE()
-    //end::init[]
+    // tag::init[]
+    fun nonNegativeInt(rng: RNG): Pair<Int, RNG> {
+        val (i1, rng2) = rng.nextInt()
+        return (if (i1 < 0) -(i1 + 1) else i1) to rng2
+    }
+    // end::init[]
 
     "nonNegativeInt" should {
 
@@ -20,7 +20,7 @@ class Exercise1 : WordSpec({
             override fun nextInt(): Pair<Int, RNG> = TODO()
         }
 
-        "!return 0 if nextInt() yields 0" {
+        "return 0 if nextInt() yields 0" {
 
             val rng0 = object : RNG {
                 override fun nextInt(): Pair<Int, RNG> =
@@ -30,7 +30,7 @@ class Exercise1 : WordSpec({
             nonNegativeInt(rng0) shouldBe (0 to unusedRng)
         }
 
-        "!return Int.MAX_VALUE when nextInt() yields Int.MAX_VALUE" {
+        "return Int.MAX_VALUE when nextInt() yields Int.MAX_VALUE" {
 
             val rngMax = object : RNG {
                 override fun nextInt(): Pair<Int, RNG> =
@@ -40,7 +40,7 @@ class Exercise1 : WordSpec({
             nonNegativeInt(rngMax) shouldBe (Int.MAX_VALUE to unusedRng)
         }
 
-        "!return Int.MAX_VALUE when nextInt() yields Int.MIN_VALUE" {
+        "return Int.MAX_VALUE when nextInt() yields Int.MIN_VALUE" {
 
             val rngMin = object : RNG {
                 override fun nextInt(): Pair<Int, RNG> =
@@ -50,7 +50,7 @@ class Exercise1 : WordSpec({
             nonNegativeInt(rngMin) shouldBe (Int.MAX_VALUE to unusedRng)
         }
 
-        "!return 0 when nextInt() yields -1" {
+        "return 0 when nextInt() yields -1" {
 
             val rngNeg = object : RNG {
                 override fun nextInt(): Pair<Int, RNG> =
