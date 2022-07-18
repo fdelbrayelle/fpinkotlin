fbcc46c31d4a940dc9b14c0379ddc30876501d57
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-18 16:31:59 +0200

    feat: exercise 6.4

--- src/test/kotlin/chapter6/exercises/ex4/listing.kt
+++ src/test/kotlin/chapter6/exercises/ex4/listing.kt
@@ -1,25 +1,29 @@
 package chapter6.exercises.ex4
 
 // import chapter3.Cons
-import chapter3.List
 // import chapter3.Nil
+import chapter3.Cons
+import chapter3.List
+import chapter3.Nil
 import chapter6.RNG
 import chapter6.rng1
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
-//TODO: Enable tests by removing `!` prefix
+// TODO: Enable tests by removing `!` prefix
 class Exercise4 : WordSpec({
 
-    //tag::init[]
+    // tag::init[]
     fun ints(count: Int, rng: RNG): Pair<List<Int>, RNG> =
-
-        SOLUTION_HERE()
-    //end::init[]
+        if (count > 0) {
+            val (i, rng2) = rng.nextInt()
+            val (xi, rng3) = ints(count - 1, rng2)
+            Cons(i, xi) to rng3
+        } else Nil to rng
+    // end::init[]
 
     "ints" should {
-        "!generate a list of ints of a specified length" {
+        "generate a list of ints of a specified length" {
 
             ints(5, rng1) shouldBe (List.of(1, 1, 1, 1, 1) to rng1)
         }
