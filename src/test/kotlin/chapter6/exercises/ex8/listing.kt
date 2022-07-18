5e890432d962786fdc1fce571f96bb7a31f3a343
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-18 17:34:54 +0200

    feat: exercise 6.8

--- src/test/kotlin/chapter6/exercises/ex8/listing.kt
+++ src/test/kotlin/chapter6/exercises/ex8/listing.kt
@@ -2,30 +2,36 @@ package chapter6.exercises.ex8
 
 import chapter6.Rand
 import chapter6.rng1
-// import chapter6.solutions.ex1.nonNegativeInt
+import chapter6.solutions.ex1.nonNegativeInt
 import chapter6.unit
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
-//TODO: Enable tests by removing `!` prefix
+// TODO: Enable tests by removing `!` prefix
 class Exercise8 : WordSpec({
 
-    //tag::init[]
-    fun <A, B> flatMap(f: Rand<A>, g: (A) -> Rand<B>): Rand<B> =
+    // tag::init[]
+    fun <A, B> flatMap(f: Rand<A>, g: (A) -> Rand<B>): Rand<B> = { rng ->
+        val (a, rng2) = f(rng)
+        g(a)(rng2)
+    }
+    // end::init[]
 
-        SOLUTION_HERE()
-    //end::init[]
-
-    fun nonNegativeIntLessThan(n: Int): Rand<Int> = SOLUTION_HERE()
+    fun nonNegativeIntLessThan(n: Int): Rand<Int> =
+        flatMap(::nonNegativeInt) { i ->
+            val mod = i % n
+            if (i + (n - 1) - mod >= 0) unit(mod)
+            else nonNegativeIntLessThan(n)
+        }
 
     "flatMap" should {
-        "!pass along an RNG" {
+        "pass along an RNG" {
 
             val result =
                 flatMap(
                     unit(1),
-                    { i -> unit(i.toString()) })(rng1)
+                    { i -> unit(i.toString()) }
+                )(rng1)
 
             result.first shouldBe "1"
             result.second shouldBe rng1
@@ -33,7 +39,7 @@ class Exercise8 : WordSpec({
     }
 
     "nonNegativeIntLessThan" should {
-        "!return a non negative int less than n" {
+        "return a non negative int less than n" {
 
             val result =
                 nonNegativeIntLessThan(10)(rng1)
