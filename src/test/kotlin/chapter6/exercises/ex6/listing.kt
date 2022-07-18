640d66e8b0da88ab27dfd15111dbed9571d1a263
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-18 17:00:49 +0200

    feat: exercise 6.6

--- src/test/kotlin/chapter6/exercises/ex6/listing.kt
+++ src/test/kotlin/chapter6/exercises/ex6/listing.kt
@@ -1,35 +1,38 @@
 package chapter6.exercises.ex6
 
 // import chapter6.RNG
+import chapter6.RNG
 import chapter6.Rand
 import chapter6.rng1
 import chapter6.unit
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
-//TODO: Enable tests by removing `!` prefix
+// TODO: Enable tests by removing `!` prefix
 class Exercise6 : WordSpec({
 
-    //tag::init[]
+    // tag::init[]
     fun <A, B, C> map2(
         ra: Rand<A>,
         rb: Rand<B>,
         f: (A, B) -> C
-    ): Rand<C> =
-
-        SOLUTION_HERE()
-    //end::init[]
+    ): Rand<C> = { rng: RNG ->
+        val (a, rng2) = ra(rng)
+        val (b, rng3) = rb(rng2)
+        f(a, b) to rng3
+    }
+    // end::init[]
 
     "map2" should {
-        "!combine the results of two actions" {
+        "combine the results of two actions" {
 
             val combined: Rand<String> =
                 map2(
                     unit(1.0),
                     unit(1), { d, i ->
                         ">>> $d double; $i int"
-                    })
+                    }
+                )
 
             combined(rng1).first shouldBe ">>> 1.0 double; 1 int"
         }
