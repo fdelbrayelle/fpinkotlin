b1804c8d3056c5b9a5e9d31267996220a891de7c
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-18 17:19:14 +0200

    feat: exercise 6.7

--- src/test/kotlin/chapter6/exercises/ex7/listing.kt
+++ src/test/kotlin/chapter6/exercises/ex7/listing.kt
@@ -1,38 +1,50 @@
 package chapter6.exercises.ex7
 
-// import chapter3.Cons
+import chapter3.Cons
 import chapter3.List
-// import chapter3.Nil
-// import chapter3.solutions.foldRight
+import chapter3.Nil
+import chapter3.foldRight
 import chapter6.RNG
 import chapter6.Rand
 import chapter6.rng1
-// import chapter6.solutions.ex6.map2
+import chapter6.solutions.ex6.map2
 import chapter6.unit
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
-//TODO: Enable tests by removing `!` prefix
+// TODO: Enable tests by removing `!` prefix
 class Exercise7 : WordSpec({
 
-    //tag::init[]
-    fun <A> sequence(fs: List<Rand<A>>): Rand<List<A>> =
+    // tag::init[]
+    fun <A> sequence(fs: List<Rand<A>>): Rand<List<A>> = { rng ->
+        when (fs) {
+            is Nil -> unit(List.empty<A>())(rng)
+            is Cons -> {
+                val (a, nrng) = fs.head(rng)
+                val (xa, frng) = sequence(fs.tail)(nrng)
+                Cons(a, xa) to frng
+            }
+        }
+    }
+    // end::init[]
 
-        SOLUTION_HERE()
-    //end::init[]
-
-    //tag::init2[]
+    // tag::init2[]
     fun <A> sequence2(fs: List<Rand<A>>): Rand<List<A>> =
+        foldRight(fs, unit(List.empty())) { f, acc ->
+            map2(f, acc) { h, t -> Cons(h, t) }
+        }
+    // end::init2[]
 
-        SOLUTION_HERE()
-    //end::init2[]
-
-    fun ints2(count: Int, rng: RNG): Pair<List<Int>, RNG> = SOLUTION_HERE()
+    fun ints2(count: Int, rng: RNG): Pair<List<Int>, RNG> {
+        fun go(c: Int): List<Rand<Int>> =
+            if (c == 0) Nil
+            else Cons({ rng -> 1 to rng }, go(c - 1))
+        return sequence2(go(count))(rng)
+    }
 
     "sequence" should {
 
-        "!combine the results of many actions using recursion" {
+        "combine the results of many actions using recursion" {
 
             val combined: Rand<List<Int>> =
                 sequence(
@@ -48,7 +60,7 @@ class Exercise7 : WordSpec({
                 List.of(1, 2, 3, 4)
         }
 
-        """!combine the results of many actions using
+        """combine the results of many actions using
             foldRight and map2""" {
 
             val combined2: Rand<List<Int>> =
@@ -67,7 +79,7 @@ class Exercise7 : WordSpec({
     }
 
     "ints" should {
-        "!generate a list of ints of a specified length" {
+        "generate a list of ints of a specified length" {
             ints2(4, rng1).first shouldBe
                 List.of(1, 1, 1, 1)
         }
