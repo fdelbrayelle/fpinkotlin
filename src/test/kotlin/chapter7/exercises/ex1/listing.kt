7dd984d3c57ca78d26bb9eccc219cc3fc9ebf4e9
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-19 19:13:13 +0200

    feat: exercise 7.1

--- src/test/kotlin/chapter7/exercises/ex1/listing.kt
+++ src/test/kotlin/chapter7/exercises/ex1/listing.kt
@@ -1,25 +1,23 @@
 package chapter7.exercises.ex1
 
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 class Par<A>(val get: A)
 
 fun <A> unit(a: () -> A): Par<A> = Par(a())
 
-//tag::init[]
+// tag::init[]
 fun <A, B, C> map2(
     sum: Par<A>,
     sum1: Par<B>,
     function: (A, B) -> C
 ): Par<C> =
-
-    SOLUTION_HERE()
-//end::init[]
+    unit({ function(sum.get, sum1.get) })
+// end::init[]
 
 class Solution1 : WordSpec({
     "Par.map2" should {
-        """!declare a valid signature that combines two Pars by
+        """declare a valid signature that combines two Pars by
             applying another function""" {
             map2(unit { 1 }, unit { 2 }) { i, j -> i + j }
         }
