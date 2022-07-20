071cf0edc072f1784a665f252c61e5de5b0d8236
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-20 13:30:50 +0200

    feat: exercise 7.5

--- src/test/kotlin/chapter7/exercises/ex5/listing.kt
+++ src/test/kotlin/chapter7/exercises/ex5/listing.kt
@@ -3,14 +3,24 @@ package chapter7.exercises.ex5
 // import chapter7.sec1.splitAt
 // import chapter7.solutions.ex3.TimedMap2Future
 // import java.util.concurrent.Callable
-import utils.SOLUTION_HERE
+import chapter7.sec1.splitAt
+import chapter7.solutions.ex5.Pars
+import chapter7.solutions.ex5.Pars.head
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Future
 
 typealias Par<A> = (ExecutorService) -> Future<A>
 
-//tag::init1[]
+// tag::init1[]
 fun <A> sequence(ps: List<Par<A>>): Par<List<A>> =
-
-    SOLUTION_HERE()
-//end::init1[]
+    when {
+        ps.isEmpty() -> Pars.unit(Pars.Nil)
+        ps.size == 1 -> Pars.map(ps.head) { listOf(it) }
+        else -> {
+            val (l, r) = ps.splitAt(ps.size / 2)
+            Pars.map2(Pars.sequence(l), Pars.sequence(r)) { la, lb ->
+                la + lb
+            }
+        }
+    }
+// end::init1[]
