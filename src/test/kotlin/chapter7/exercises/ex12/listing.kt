02e03869e98b9333bd5cfacddab01be43d13ff31
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-20 20:42:06 +0200

    feat: exercise 7.12

--- src/test/kotlin/chapter7/exercises/ex12/listing.kt
+++ src/test/kotlin/chapter7/exercises/ex12/listing.kt
@@ -1,13 +1,12 @@
 package chapter7.exercises.ex12
 
-import utils.SOLUTION_HERE
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Future
 
 typealias Par<A> = (ExecutorService) -> Future<A>
 
-//tag::init[]
-fun <A, B> chooser(pa: Par<A>, choices: (A) -> Par<B>): Par<B> =
-
-    SOLUTION_HERE()
-//end::init[]
+// tag::init[]
+fun <A, B> chooser(pa: Par<A>, choices: (A) -> Par<B>): Par<B> = { es ->
+    choices(pa.invoke(es).get()).invoke(es)
+}
+// end::init[]
