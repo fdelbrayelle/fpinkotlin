6a04189e4fd0c780a08d744c2d6689823e58871e
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-20 20:41:22 +0200

    feat: exercise 7.10

--- src/test/kotlin/chapter7/exercises/ex10/listing.kt
+++ src/test/kotlin/chapter7/exercises/ex10/listing.kt
@@ -1,14 +1,18 @@
 package chapter7.exercises.ex10
 
 // import chapter7.sec3.Pars
-import utils.SOLUTION_HERE
+import chapter7.solutions.ex10.map
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Future
 
 typealias Par<A> = (ExecutorService) -> Future<A>
 
 fun <A> choiceN(n: Par<Int>, choices: List<Par<A>>): Par<A> =
-    SOLUTION_HERE()
+    { es: ExecutorService ->
+        choices.get(n(es).get()).invoke(es)
+    }
 
 fun <A> choice(cond: Par<Boolean>, t: Par<A>, f: Par<A>): Par<A> =
-    SOLUTION_HERE()
+    { es: ExecutorService ->
+        choiceN(map(cond) { if (it) 1 else 0 }, listOf(t, f))(es)
+    }
