929cf550e5ae643ef12879a6cbadcc755ea3342c
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-20 13:36:52 +0200

    feat: exercise 7.6

--- src/test/kotlin/chapter7/exercises/ex6/listing.kt
+++ src/test/kotlin/chapter7/exercises/ex6/listing.kt
@@ -1,6 +1,6 @@
 package chapter7.exercises.ex6
 
-import utils.SOLUTION_HERE
+import chapter7.solutions.ex6.Pars
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Future
 
@@ -10,7 +10,12 @@ typealias Par<A> = (ExecutorService) -> Future<A>
 fun <A> parFilter(
     sa: List<A>,
     f: (A) -> Boolean
-): Par<List<A>> =
-
-    SOLUTION_HERE()
+): Par<List<A>> {
+    val pars: List<Par<A>> = sa.map { Pars.lazyUnit { it } }
+    return Pars.map(Pars.sequence(pars)) { la: List<A> ->
+        la.flatMap { a ->
+            if (f(a)) listOf(a) else emptyList()
+        }
+    }
+}
 //end::init[]
