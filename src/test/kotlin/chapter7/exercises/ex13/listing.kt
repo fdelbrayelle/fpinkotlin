0254b98a861e55825dc92ceb34a82080c29e43d6
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-20 20:44:40 +0200

    feat: exercise 7.13

--- src/test/kotlin/chapter7/exercises/ex13/listing.kt
+++ src/test/kotlin/chapter7/exercises/ex13/listing.kt
@@ -1,13 +1,12 @@
 package chapter7.exercises.ex13
 
-import utils.SOLUTION_HERE
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Future
 
 typealias Par<A> = (ExecutorService) -> Future<A>
 
-//tag::init[]
-fun <A> join(a: Par<Par<A>>): Par<A> =
-
-    SOLUTION_HERE()
-//end::init[]
+// tag::init[]
+fun <A> join(a: Par<Par<A>>): Par<A> = { es ->
+    a(es).get()(es)
+}
+// end::init[]
