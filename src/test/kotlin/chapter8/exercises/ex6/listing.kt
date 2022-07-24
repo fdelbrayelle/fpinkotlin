0760ebb2c597a5c12fb6ef6e14125c0a9c965b76
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-24 11:48:54 +0200

    feat: exercise 8.6

--- src/test/kotlin/chapter8/exercises/ex6/listing.kt
+++ src/test/kotlin/chapter8/exercises/ex6/listing.kt
@@ -2,19 +2,16 @@ package chapter8.exercises.ex6
 
 import chapter8.RNG
 import chapter8.State
-import utils.SOLUTION_HERE
 
-//tag::init[]
+// tag::init[]
 data class Gen<A>(val sample: State<RNG, A>) {
 
     companion object {
         fun <A> listOfN(gn: Gen<Int>, ga: Gen<A>): Gen<List<A>> =
-
-            SOLUTION_HERE()
+            gn.flatMap { Gen(State.sequence(List(it) { ga.sample })) }
     }
 
     fun <B> flatMap(f: (A) -> Gen<B>): Gen<B> =
-
-        SOLUTION_HERE()
+        Gen(sample.flatMap { f(it).sample })
 }
-//end::init[]
+// end::init[]
