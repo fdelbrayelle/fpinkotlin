02efca3f9c052f71d96c771630d20b54bdf0db83
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-24 11:55:46 +0200

    feat: exercise 8.8

--- src/test/kotlin/chapter8/exercises/ex8/listing.kt
+++ src/test/kotlin/chapter8/exercises/ex8/listing.kt
@@ -2,19 +2,25 @@ package chapter8.exercises.ex8
 
 import chapter8.RNG
 import chapter8.State
-import utils.SOLUTION_HERE
+import chapter8.double
+import kotlin.math.absoluteValue
 
 data class Gen<A>(val sample: State<RNG, A>) {
     companion object {
 
-        //tag::init[]
+        // tag::init[]
         fun <A> weighted(
             pga: Pair<Gen<A>, Double>,
             pgb: Pair<Gen<A>, Double>
-        ): Gen<A> =
-
-            SOLUTION_HERE()
-        //end::init[]
+        ): Gen<A> {
+            val (ga, p1) = pga
+            val (gb, p2) = pgb
+            val prob =
+                p1.absoluteValue /
+                    (p1.absoluteValue + p2.absoluteValue)
+            return Gen(State { rng: RNG -> double(rng) })
+                .flatMap { d -> if (d < prob) ga else gb }
+        }
     }
 
     fun <B> flatMap(f: (A) -> Gen<B>): Gen<B> =
