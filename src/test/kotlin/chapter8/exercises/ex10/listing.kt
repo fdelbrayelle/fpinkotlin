0ccd2f9bf98d676bf5e5af4485cb6929dd456312
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-24 14:25:33 +0200

    feat: exercise 8.10

--- src/test/kotlin/chapter8/exercises/ex10/listing.kt
+++ src/test/kotlin/chapter8/exercises/ex10/listing.kt
@@ -2,16 +2,13 @@ package chapter8.exercises.ex10
 
 import chapter8.RNG
 import chapter8.State
-import utils.SOLUTION_HERE
 
 data class SGen<A>(val forSize: (Int) -> Gen<A>)
 
 data class State<S, out A>(val run: (S) -> Pair<A, S>)
 
-//tag::init[]
+// tag::init[]
 data class Gen<A>(val sample: State<RNG, A>) {
-    fun unsized(): SGen<A> =
-
-        SOLUTION_HERE()
+    fun unsized(): SGen<A> = SGen { this }
 }
-//end::init[]
+// end::init[]
