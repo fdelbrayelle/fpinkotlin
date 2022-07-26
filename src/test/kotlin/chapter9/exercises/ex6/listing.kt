c41c6f92d835e93bdc8a086c71080f5dcaa7981b
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-26 20:37:23 +0200

    feat: exercise 9.6

--- src/test/kotlin/chapter9/exercises/ex6/listing.kt
+++ src/test/kotlin/chapter9/exercises/ex6/listing.kt
@@ -3,15 +3,16 @@ package chapter9.exercises.ex6
 import chapter9.solutions.final.ParseError
 import chapter9.solutions.final.Parser
 import chapter9.solutions.final.ParserDsl
-import utils.SOLUTION_HERE
 
 abstract class Listing : ParserDsl<ParseError>() {
     init {
 
-        //tag::init1[]
-        val parser: Parser<Int> =
-
-            SOLUTION_HERE()
-        //end::init1[]
+        // tag::init1[]
+        val parser: Parser<Int> = regex("[0-9]+")
+            .flatMap { digit: String ->
+                val int = digit.toInt()
+                listOfN(int, char('a')).map { int }
+            }
+        // end::init1[]
     }
 }
