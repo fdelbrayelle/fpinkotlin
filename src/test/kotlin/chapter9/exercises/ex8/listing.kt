9805989b8c35d3fcb873572fca340cf059d4f61b
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-26 20:40:11 +0200

    feat: exercise 9.8

--- src/test/kotlin/chapter9/exercises/ex8/listing.kt
+++ src/test/kotlin/chapter9/exercises/ex8/listing.kt
@@ -3,15 +3,13 @@ package chapter9.exercises.ex8
 import chapter9.solutions.final.ParseError
 import chapter9.solutions.final.Parser
 import chapter9.solutions.final.ParserDsl
-import utils.SOLUTION_HERE
 
 abstract class Listing : ParserDsl<ParseError>() {
 
     init {
-        //tag::init1[]
+        // tag::init1[]
         fun <A, B> map(pa: Parser<A>, f: (A) -> B): Parser<B> =
-
-            SOLUTION_HERE()
-        //end::init1[]
+            pa.flatMap { a -> succeed(f(a)) }
+        // end::init1[]
     }
 }
