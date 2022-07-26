a44de933ef271c6d95923ba86fe170e4750ab61e
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-26 18:09:58 +0200

    feat: exercise 9.1

--- src/test/kotlin/chapter9/exercises/ex1/listing.kt
+++ src/test/kotlin/chapter9/exercises/ex1/listing.kt
@@ -3,23 +3,20 @@ package chapter9.exercises.ex1
 import chapter9.ParseError
 import chapter9.solutions.final.Parser
 import chapter9.solutions.final.ParserDsl
-import utils.SOLUTION_HERE
 
 abstract class Listing : ParserDsl<ParseError>() {
 
-    //tag::init1[]
+    // tag::init1[]
     override fun <A, B, C> map2(
         pa: Parser<A>,
         pb: () -> Parser<B>,
         f: (A, B) -> C
     ): Parser<C> =
+        (pa product pb).map { (a, b) ->  f(a, b) }
+    // end::init1[]
 
-        SOLUTION_HERE()
-    //end::init1[]
-
-    //tag::init2[]
+    // tag::init2[]
     override fun <A> many1(p: Parser<A>): Parser<List<A>> =
-
-        SOLUTION_HERE()
-    //end::init2[]
+        map2(p, { p.many() }) { a, b -> listOf(a) + b }
+    // end::init2[]
 }
