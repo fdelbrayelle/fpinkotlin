e43f4fa01bbf078345972bcb097deb16acc1cc6c
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-27 18:27:33 +0200

    feat: exercise 9.10

--- src/test/kotlin/chapter9/exercises/ex10/listing.kt
+++ src/test/kotlin/chapter9/exercises/ex10/listing.kt
@@ -1,7 +1,13 @@
 package chapter9.exercises.ex10
 
-import chapter9.solutions.final.Parser
+import chapter9.exercises.ex5.Parser
 
 interface Parsers {
+    // tag::init1[]
+    fun <A> furthest(pa: Parser<A>): Parser<A>
+    // end::init1[]
 
+    // tag::init2[]
+    fun <A> latest(pa: Parser<A>): Parser<A>
+    // end::init2[]
 }
