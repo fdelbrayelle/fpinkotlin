007bdc67329a8715116338672bda64eb9f701cdc
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-28 15:25:27 +0200

    feat: exercise 9.12

--- src/test/kotlin/chapter9/exercises/ex12/listing.kt
+++ src/test/kotlin/chapter9/exercises/ex12/listing.kt
@@ -1,12 +1,15 @@
 package chapter9.exercises.ex12
 
+import arrow.core.None
 import arrow.core.Option
+import arrow.core.Some
+import chapter9.solutions.final.Failure
 import chapter9.solutions.final.Location
 import chapter9.solutions.final.ParseError
 import chapter9.solutions.final.Parser
 import chapter9.solutions.final.ParserDsl
 import chapter9.solutions.final.State
-import utils.SOLUTION_HERE
+import chapter9.solutions.final.Success
 
 abstract class Solution : ParserDsl<ParseError>() {
 
@@ -18,9 +21,19 @@ abstract class Solution : ParserDsl<ParseError>() {
 
     private fun State.advanceBy(i: Int): Location = TODO()
 
-    //tag::init1[]
+    // tag::init1[]
     override fun string(s: String): Parser<String> =
-
-        SOLUTION_HERE()
-    //end::init1[]
+        { state: State ->
+            when (
+                val idx =
+                    firstNonMatchingIndex(state.input, s, state.offset)
+            ) {
+                is None -> Success(s, s.length)
+                is Some -> Failure(
+                    state.advanceBy(idx.t).toError("'$s'"),
+                    idx.t != 0
+                )
+            }
+        }
+    // end::init1[]
 }
